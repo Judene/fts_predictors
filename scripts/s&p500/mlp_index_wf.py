dfcd252e7d89a503093904623945476be7aead51
Judene <62376212+Judene@users.noreply.github.com>
2021-12-01 14:54:14 +0200

    Update and rename mlp_bond_wf.py.txt to mlp_index_wf.py

--- scripts/s&p500/mlp_index_wf.py
+++ scripts/s&p500/mlp_index_wf.py
@@ -17,24 +17,24 @@ pathlib.Path(os.path.join(os.path.dirname(os.getcwd()), "..", "data")).mkdir(par
 data_path = os.path.join(os.path.dirname(os.getcwd()), "..", "data")
 
 # Check if file exists
-if not os.path.exists(os.path.join(data_path, "local_etfs_close.csv")):
+if not os.path.exists(os.path.join(data_path, "s&p500_index.csv")):
     raise ValueError("No data in data folder!")
 
-# Get bond data
-bond_etf_data = pd.read_csv(os.path.join(data_path, "local_etfs_close.csv"), index_col=0)
-bond_etf_data = bond_etf_data["GLD"].to_frame().ffill().dropna()
-dates = bond_etf_data
+# Get index data
+index_data = pd.read_csv(os.path.join(data_path, "s&p500_index.csv"), index_col=0)
+index_data = index_data.to_frame().ffill().dropna()
+dates = index_data
 
 n_features = 2
 
-bond_etf_data = series_to_supervised(bond_etf_data, n_in=n_features, n_out=1)
-input_data = bond_etf_data.drop(['var1(t)'], axis=1)
-output_data = bond_etf_data.drop(['var1(t-2)', 'var1(t-1)'], axis=1)
+index_data = series_to_supervised(index_data, n_in=n_features, n_out=1)
+input_data = index_data.drop(['var1(t)'], axis=1)
+output_data = index_data.drop(['var1(t-2)', 'var1(t-1)'], axis=1)
 
 
 # Create MLP model
 mlp_model = MultiLayerPerceptron(
-    name="mlp_bond_wf",
+    name="mlp_index_wf",
     num_inputs=n_features,
     num_outputs=1,
     # If true, training info is outputted to stdout
